113e40895d062ceeb96c2818b787aa2eb83a442e
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-24 17:17:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,14 @@
-+var playlist = new Object({keys: '0'});
-+
-+function updatePlaylist(playlist, artistName, songTitle) {
-+  playlist[artistName] = songTitle
-+  return playlist
-+}
-+
-+function removeFromPlaylist(playlist, artistName) {
-+  delete playlist[artistName]
-+  return playlist
-+}
+var playlist = {
+  MyRiot: 'Sen',
+  Roman: 'Something'
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+} 
