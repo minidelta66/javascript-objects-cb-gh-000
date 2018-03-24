e781b714f4cdaf3a2341f4538f73db659b19b0aa
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-24 17:09:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
-var playlist = {
- MyRiot: 'Sen',
-};
+var playlist = new Object {[artistName: songTitle]};
+
 
 function updatePlaylist(playlist, artistName, songTitle) {
 playlist.artistName = songTitle
