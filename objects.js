31d17868aebd31213f208b6da0242314d4770c8a
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-24 17:15:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,11 @@
-var playlist = {
-  artistName: "songTitle"};
-
-
-function updatePlaylist(playlist, artistName, songTitle) {
-playlist.artistName = songTitle;
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  return delete playlist.artistName;
-}
++var playlist = new Object({keys: '0'});
++
++function updatePlaylist(playlist, artistName, songTitle) {
++  playlist[artistName] = songTitle
++  return playlist
++}
++
++function removeFromPlaylist(playlist, artistName) {
++  delete playlist[artistName]
++  return playlist
++}
